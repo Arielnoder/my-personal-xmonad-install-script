9bcfc70ea326eedac4e86dad358c58ebbf7f5b00
Arielnoder <74354077+Arielnoder@users.noreply.github.com>
2021-10-06 22:36:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
-sudo pacman -S xmonad xmonad-contrib xmobar kitty nitrogen picom; mkdir ~/.xmonad; touch ~/.xmobarrc; git clone https://github.com/Arielnoder/xmonad.git; git clone https://github.com/Arielnoder/xmobar.git; cp ~/xmobar/xmobarrc .xmobarrc ; cp ~/xmonad/xmonad.hs ~/.xmonad/ 
+sudo pacman -S xmonad xmonad-contrib xmobar kitty nitrogen picom dmenu ttf-ubuntu-font-family;
+mkdir ~/.xmonad;
+touch ~/.xmobarrc;
+git clone https://github.com/Arielnoder/xmonad.git;
+git clone https://github.com/Arielnoder/xmobar.git;
+cp ~/xmobar/xmobarrc .xmobarrc;
+cp ~/xmonad/xmonad.hs ~/.xmonad/ 
 
